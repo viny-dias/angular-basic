87519a55f502cb9c67243d5dff5dab6e1d75e4dc
VINICIUS DIAS MOTA DE OLIVEIRA <52006820885@cmdo.policiamilitar.sp.gov.br>
2024-09-03 11:37:01 -0300

    configuring the App component for module

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { type NewTaskData } from '../task/task.model';
 import { TasksService } from '../tasks.service';
 
 @Component({
